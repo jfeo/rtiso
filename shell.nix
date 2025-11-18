e86d11c01e5fe222a5c1174ef3b3a9924e23a4ba
Jens Feodor Nielsen <jens@feodor.dk>
2025-11-18 14:39:42 +0100

    nix: add shell.nix and update Makefile to accept CFLAGS_EXTRA
    To build on Nix, currently SOIL has to be specifically included and
    add to the library search path.

---
+++ shell.nix
@@ -0,0 +1,15 @@
+with import <nixpkgs> { };
+
+stdenv.mkDerivation {
+  name = "c";
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [
+    glfw
+    glew
+    libGL
+    libGLU
+    soil
+    freetype
+  ];
+  shellHook = "";
+}
