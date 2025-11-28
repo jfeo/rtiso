507bc5e4e1193e47841e0f2d596e91defe4fd37a
Jens Feodor Nielsen <jens@feodor.dk>
2025-11-28 13:03:21 +0100

    Test entrypoint print overall status of test run

--- src/test.c
+++ src/test.c
@@ -2,10 +2,17 @@
 
 #include "coord_test.c"
 #include "unit_test.c"
+#include <stdio.h>
 
 int main() {
   int failed = 0;
-  failed |= coord_test_suite();
-  failed |= unit_test_suite();
+  failed += coord_test_suite();
+  failed += unit_test_suite();
+  printf("[==========] Total result:\n");
+  if (failed) {
+    printf("\n%d TOTAL FAILED TEST(s).\n", failed);
+  } else {
+    printf("[  PASSED  ]\n");
+  }
   return failed;
 }
